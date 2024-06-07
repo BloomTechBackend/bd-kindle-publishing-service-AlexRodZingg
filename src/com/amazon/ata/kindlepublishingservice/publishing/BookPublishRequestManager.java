e8acbb0132d47608580e18981609d6819e046702
Alexander Rodriguez-Zingg <alexrodriguezzingg@icloud.com>
2024-06-07 16:27:55 -0600

    Completed project

--- src/com/amazon/ata/kindlepublishingservice/publishing/BookPublishRequestManager.java
+++ src/com/amazon/ata/kindlepublishingservice/publishing/BookPublishRequestManager.java
@@ -2,7 +2,6 @@ package com.amazon.ata.kindlepublishingservice.publishing;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
